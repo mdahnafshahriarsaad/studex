3ddf39d16db770c99b2dd5bdf2c34bb47c6470a3
Md Ahnaf Shahriar Saad <>
2026-07-29 18:24:51 +0600

    fix: change base URL from '/studex/' to '/' for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/studex/' : '/',
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
